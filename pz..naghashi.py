92edc9511bb7556155fcf1a9360abc1db0877658
soroush09z <hasanisoroush97@gmail.com>
2024-10-08 18:05:39 +0330

    lotfa hemayat konid

---
+++ pz..naghashi.py
@@ -0,0 +1,19 @@
+import turtle as t
+t.shape('turtle')
+t.pencolor('blue')
+t.speed(3)
+t.pensize(3)
+t.bgcolor('red')
+for x in range(8):
+    for z in range(8):
+        t.fd(100)
+        t.rt(45)
+    t.rt(45)
+t.hideturtle()
+
+a=input()
+if a==100000000:
+    print('idfhheu')
+
+else:
+    print()
